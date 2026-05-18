fbb1a5571f77f690a90569bf1eceda5f303e6c1b
Jatanwani <jjatan220@gmail.com>
2026-05-19 00:59:41 +0530

    Add CapitalPage component with InvestorDashboard

---
+++ app/dashboard/capital/page.tsx
@@ -0,0 +1,9 @@
+import { InvestorDashboard } from '@/components/capital/InvestorDashboard'
+
+export default function CapitalPage() {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <InvestorDashboard />
+    </div>
+  )
+}
